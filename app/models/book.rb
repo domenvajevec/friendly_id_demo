fe24eca47911b115c9b91245eacef67a1a9a16ea
domenvajevec <domenvajevec@gmail.com>
2014-08-05 13:31:36 -0700

    implement  carrier_wave gem functionality

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ActiveRecord::Base
   extend FriendlyId
-
   friendly_id :name, use: :slugged
+
+  mount_uploader :cover, CoverUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804201817) do
+ActiveRecord::Schema.define(version: 20140805202216) do
 
   create_table "books", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140804201817) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.string   "cover"
   end
 
   add_index "books", ["slug"], name: "index_books_on_slug", unique: true
